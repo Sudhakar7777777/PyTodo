8c34608bf95267f245586bedf7aedc7420d6d0f8
Sudhakar Balakrishnan <>
2019-09-03 01:37:37 -0700

    add users controller, integrated with mongodb

--- index.py
+++ index.py
@@ -3,12 +3,13 @@ import os
 import sys
 import requests
 from flask import jsonify, request, make_response, send_from_directory
-from modules import logger
-from modules.app import app
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 os.environ.update({'ROOT_PATH': ROOT_PATH})
 sys.path.append(os.path.join(ROOT_PATH, 'modules'))
+
+from modules.logger import logger
+from modules.app import app
 
 # create a logger object to log the info and debug
 LOG = logger.get_root_logger(os.environ.get('ROOT_LOGGER', 'root'), filename=os.path.join(ROOT_PATH, 'output.log'))
